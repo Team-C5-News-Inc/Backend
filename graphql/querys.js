e2cfa778c0fcfdfc748b4ba85713b7c0d947747d
johan-avila <johan-avila@outlook.com>
2020-10-31 21:44:04 -0600

    succces search

--- graphql/querys.js
+++ graphql/querys.js
@@ -24,9 +24,7 @@ module.exports = {
     try {
       mongodb = await connectMongoDb()
 
-      /* Creted Index if not exist */
-      await mongodb.collection('news').createIndex({ title: 'text', subtitle: 'text', body: 'text' })
-      /* Creted Index if not exist */
+     
 
       news = await mongodb.collection('news')
         .find({ $text: { $search: keyword } }
