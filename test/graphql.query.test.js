33547825c477e2f65fe4a1baaeb65abede4db3ab
johan-avila <johan-avila@outlook.com>
2020-10-31 20:09:20 -0600

    Other commit

--- test/graphql.query.test.js
+++ test/graphql.query.test.js
@@ -1,3 +1,4 @@
 const assert = require('assert')
 
 const { getNews, searchNews } = require('../graphql/querys')
+getNews()
---
+++ utils/mocks/mongoGraphql.js
@@ -0,0 +1,21 @@
+async function connectDB () {
+    let connect;
+
+    if (connect) return connect
+
+    let client
+    try {
+      client = await MongoClient.connect(MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      })
+      connect = client.db(DB_NAME)
+    } catch (error) {
+      console.error('could not connect to db', MONGO_URI, error)
+      process.exit(1)
+    }
+
+  return connect
+}
+
+module.exports = connectDB
